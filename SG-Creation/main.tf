55669a57ae9be62a096ec960771f28c73ade64f1
oberoi1984 <41862558+oberoi1984@users.noreply.github.com>
2024-10-25 20:51:34 +0530

    Update main.tf

--- SG-Creation/main.tf
+++ SG-Creation/main.tf
@@ -1,20 +1,32 @@
 provider "aws" {
   region  = "ap-south-1" # Replace with your desired region
   profile = "default"    # Replace with your actual AWS profile if you want to use a named profile
+}
+resource "aws_security_group" "custom_sg" {
+  name        = var.name
+  description = var.description
+  vpc_id      = var.vpc_id
 
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
 
+  dynamic "egress" {
+    for_each = var.egress_rules
+    content {
+      from_port   = egress.value.from_port
+      to_port     = egress.value.to_port
+      protocol    = egress.value.protocol
+      cidr_blocks = egress.value.cidr_blocks
+    }
+  }
+
+  tags = var.tags
 }
 
-
-# Sourcing a Terraform module from GitHub
-module "Test_module" {
-  source = "github.com/oberoi1984/terraform-module-test/SG-Creation"
-  #source = "git@github.com:oberoi1984/terraform-module-test.git"
-  name          = var.name
-  description   = var.description
-  vpc_id        = var.vpc_id
-  ingress_rules = var.ingress_rules
-  egress_rules  = var.egress_rules
-  tags          = var.tags
-
-}
